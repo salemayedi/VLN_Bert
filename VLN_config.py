f23c99ab29a8d46a2c4369f683ecd05946bcf170
salemayedi <salem.ayedi93@gmail.com>
2020-07-23 17:13:48 +0200

    DataLoader + Training Action Grouding

--- VLN_config.py
+++ VLN_config.py
@@ -4,7 +4,7 @@ config = SimpleNamespace(
     bert_model="bert-base-uncased",
     config_file="config/bert_base_6layer_6conect.json",
     # max_seq_length=101,
-    train_batch_size=1,
+    train_batch_size=2,
     do_lower_case=True,
     predict_feature=False,
     seed=42,
@@ -31,12 +31,12 @@ config = SimpleNamespace(
     warmup_proportion=0.1,
     # feature extractor
     threshold_similarity=0.7,
-    best_features=10,
+    best_features=5, # is n_boxes
     max_temporal_memory_buffer=3,  # the last pic is included, so you are basically only comparing to max_temporal_memory_buffer-1
     # track temporal features
     track_temporal_features=True,
     mean_layer=False,
                 # if true output feature extractor embedding [m * 2048];
                 # if False  output feature extractor embedding [ 2048];
-    num_key_frames=3,
+    num_key_frames=6,
 )
